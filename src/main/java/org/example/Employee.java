890b84960402eef9bd51021473e30ff79fe9af20
Magic <isip_i.v.egorov@mpt.ru>
2026-03-26 10:27:42 +0300

    refactor_create_employee

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -5,7 +5,6 @@ class Employee {
     String position;
     int baseSalary;
 
-
     Employee(String name, String position, int baseSalary) {
         this.name = name;
         this.position = position;
@@ -22,30 +21,5 @@ class Employee {
         System.out.println("Базовая зарплата: " + baseSalary);
         System.out.println("Бонус: " + calculateBonus());
         System.out.println("------------------------");
-    }
-
-    static Employee createEmployee() {
-        return new Employee("Илья Егоров", "Сотрудник", 999990000);
-    }
-
-    static Developer createDeveloper() {
-        return new Developer("Алексей Иванов", "Разработчик", 80000, "Java");
-    }
-
-    static Manager createManager() {
-        return new Manager("Мария Сидорова", "Менеджер", 70000, 5);
-    }
-
-    static void runTask() {
-        System.out.println(" Информация о всех сотрудниках \n");
-
-        Employee emp = createEmployee();
-        emp.displayInfo();
-
-        Developer dev = createDeveloper();
-        dev.displayInfo();
-
-        Manager man = createManager();
-        man.displayInfo();
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
 public class Main {
+
+    public static void createAndDisplayEmployees() {
+        System.out.println(" Информация о всех сотрудниках \n");
+
+        Employee emp = new Employee("Илья Егоров", "Сотрудник", 999990000);
+        emp.displayInfo();
+
+        Developer dev = new Developer("Алексей Иванов", "Разработчик", 80000, "Java");
+        dev.displayInfo();
+
+        Manager man = new Manager("Мария Сидорова", "Менеджер", 70000, 5);
+        man.displayInfo();
+    }
+
     public static void main(String[] args) {
-        Employee.runTask();
+        createAndDisplayEmployees();
     }
 }
